f6657f9db7dbf518da09c45a749ee1db7f539d5a
odevpedro <pedrosschmidt2@gmail.com>
2023-09-13 15:21:39 -0300

    trabalhando com arrays

--- 1/main.go
+++ 1/main.go
@@ -1,22 +1,22 @@
 package main
 
+import "fmt"
+
+const a = "Hello, World"
+
+type ID int
+
 // inferencia de tipos
 // escopo global
 var (
-	b bool = true
-	c int
-	d string
-	e float64
+	b bool    = true
+	c int     = 10
+	d string  = "Pedro"
+	e float64 = 1.2
+	f ID      = 1
 )
 
 func main() {
 
-	a := "X"  // shortcut - usado apenas na primeira vez que a variavel é criada
-	a := "fd" // não funciona porque ela já existe
-
-	var
-	//b = 20 não posso mudar o tipo de uma variavel (fortemente tipada)
-
-	// var a string - escopo local
-	println (b)
+	fmt.Printf("O tipo de E é %T", f)
 }
